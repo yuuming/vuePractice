b8216b2195f7098454d0b175d31bfab11c85b238
Yuumi <yuuming.n@gmail.com>
2018-12-20 15:12:01 +0900

    Create HelloChild.vue

---
+++ my-project/src/components/HelloChild.vue
@@ -0,0 +1,17 @@
+<template>
+ <div>
+  <p>{{title}}</p>
+  <p>{{message}}</p>
+ </div>
+</template>
+ 
+<script>
+export default {
+  data: function () {
+    return {
+      title: 'Hello Child vue'
+    }
+  },
+  props: ['message']
+}
+</script>
