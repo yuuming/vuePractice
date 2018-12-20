b325b9caf9d5995c83d57321712d24caf4b489c5
Yuumi <yuuming.n@gmail.com>
2018-12-20 16:29:22 +0900

    Implement increment function

--- my-project/src/components/HelloWorld.vue
+++ my-project/src/components/HelloWorld.vue
@@ -1,7 +1,10 @@
 <template>
  <div>
   <p>{{title}}</p>
-  <hello-child message= Hello Child vue/>
+  <!-- <p><input v-model="input_msg"></p> -->
+<p>{{ total }}</p>
+  <!-- <hello-child :message="input_msg" /> -->
+  <hello-child v-on:increment="incrementTotal" />
  </div>
 </template>
  
@@ -10,7 +13,13 @@ import HelloChild from './HelloChild'
 export default {
   data: function () {
     return {
-      title: 'Hello world'
+      title: 'Hello world',
+      total: 0
+    }
+  },
+  methods: {
+    incrementTotal: function() {
+      this.total += 1
     }
   },
   components: {
